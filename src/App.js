7657a4a037449c40699771d8705120750083a52c
Mel <huntermelverton@gmail.com>
2020-02-25 17:48:38 -0500

    feat(cat-api): Add Cat API Display and CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      isLoaded: false,
+      index: 0
+    };
+    this.fetchImages = this.fetchImages.bind(this);
+  }
+
+  fetchImages() {
+    const url = "https://api.thecatapi.com/v1/images/search";
+    Promise.all([fetch(url), fetch(url), fetch(url), fetch(url), fetch(url)])
+
+      .then(([res1, res2, res3, res4, res5]) => {
+        return Promise.all([
+          res1.json(),
+          res2.json(),
+          res3.json(),
+          res4.json(),
+          res5.json()
+        ]);
+      })
+      .then(cats => {
+        cats.map(cat => {
+          let newArray = this.state.images.concat(cat[0].url);
+          this.setState({ images: newArray, isLoaded: true });
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+    const { isLoaded, images, index } = this.state;
+    const length = images.length - 1;
+    const handleNext = () => {
+      index === length
+        ? this.setState({ index: 0 })
+        : this.setState({ index: this.state.index + 5 });
+      this.fetchImages();
+    };
+    const handlePrevious = () =>
+      index === length
+        ? this.setState({ index: length })
+        : this.setState({ index: this.state.index - 5 });
+    return (
+      <>
+        <h4 className="cat_header">Cats are fun!</h4>
+        <div className="container">
+          {!index <= 0 && <button onClick={handlePrevious}>Back</button>}
+          <div className="cat_container">
+            {!isLoaded && (
+              <div className="not_loaded">Loading... one moment please</div>
+            )}
+            {isLoaded &&
+              images.map((cat, i) => {
+                if (i < this.state.index + 5 && i >= this.state.index) {
+                  return (
+                    <div key={i}>
+                      <img className="cat_image" src={cat} />
+                    </div>
+                  );
+                }
+              })}
+          </div>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
